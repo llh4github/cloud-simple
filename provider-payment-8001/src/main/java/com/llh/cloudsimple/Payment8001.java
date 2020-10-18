12f78fc4f53412577f891ead49b8ef8287b057cb
llh <leelinhong@outlook.com>
2020-10-18 20:17:17 +0800

    Initial commit

---
+++ provider-payment-8001/src/main/java/com/llh/cloudsimple/Payment8001.java
@@ -0,0 +1,19 @@
+package com.llh.cloudsimple;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Payment8001
+ * <p>
+ * CreatedAt: 2020-10-18 20:05
+ *
+ * @author llh
+ */
+@SpringBootApplication
+public class Payment8001 {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Payment8001.class, args);
+    }
+}
